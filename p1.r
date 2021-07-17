56bfa5fb34eb00ad4b774dada11f1ac415ec70d7
AdityaDhuri <87560794+AdityaDhuri@users.noreply.github.com>
2021-07-17 12:17:16 +0530

    Add files via upload

--- p1.r
+++ p1.r
@@ -1,1 +1,14 @@
-
+quake<-datasets:: quakes
+par(mfrow=c(2,2), mar=c(2,2,2,2), las=0, bty='o')
+plot(quakes$lat, quakes$long, col="green",type="p")
+barplot(quakes$depth, main="depth",col="orange")
+hist(quakes$stations, col="violet")
+boxplot(quakes$lat, col="yellow")
+skewness(quakes$depth)
+skewness(quakes$mag)
+plot(density(quakes$lat))
+kurtosis(quakes$depth)
+kurtosis(quakes$mag)
+var(quakes$lat)
+sd(quakes$depth, na.rm=T)
+sd(quakes$depth)
